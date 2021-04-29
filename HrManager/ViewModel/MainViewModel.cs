df3945d04957ca5a142d5539dfb537d0d8d25de1
Emmanuel Durin <emmanueldurin@gmail.com>
2021-04-29 09:53:04 +0200

    Utilisation de Task pour libérer le thread graphique

--- HrManager/ViewModel/MainViewModel.cs
+++ HrManager/ViewModel/MainViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using ViewModel;
+using ViewModel.Commands;
 
 namespace HrManager.ViewModel
 {
@@ -9,6 +13,20 @@ namespace HrManager.ViewModel
     private ObservableCollection<Person> people = new ObservableCollection<Person>();
     private Person currentPerson;
 
+    private bool isRefreshing;
+
+    public bool IsRefreshing
+    {
+      get { return isRefreshing; }
+      set
+      {
+        bool hasChanged = SetProperty(ref isRefreshing, value);
+        if (hasChanged)
+          // On émet un évt pour dire au bouton de se mettre à jour
+          RefreshCmd.FireExecuteChanged();
+      }
+    }
+    public RelayCommand RefreshCmd { get; set; }
     public ObservableCollection<Person> People
     {
       get { return people; }
@@ -28,11 +46,33 @@ namespace HrManager.ViewModel
 
     public MainViewModel()
     {
-      Refresh();
+       // on donne Refresh et CanRefresh en callback sur le bouton
+      RefreshCmd = new RelayCommand(Refresh, CanRefresh );
+      
+      // Refresh au démarrage de l'application
+      Refresh(null);
+    }
+    private bool CanRefresh(object obj)
+    {
+      return !IsRefreshing;
     }
 
-    private void Refresh()
+    private async void Refresh(object o)
     {
+      // !!! utiliser la propriété pour émettre l'évenement
+      IsRefreshing = true;
+      people.Clear();
+
+      // Pas bien bloque le thread graphique :
+      //Thread.Sleep(5000);
+
+      //Bien : car bloque l'exécution des instructions suivantes
+      // libère le thread graphique pour qu'il puisse exécuter d'autres callback
+      await Task.Delay(5000);
+      
+      // Pour éxécuter du code sur un autre thread : calcul
+      //Task.Run();
+
       var query = Enumerable.Range(1, 10)
                             .Select(i => new Person
                             {
@@ -44,6 +84,7 @@ namespace HrManager.ViewModel
       {
         people.Add(person);
       }
+      IsRefreshing = false;
     }
   }
 }
