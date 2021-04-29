0d108cd7884568351fd7ee4fac9e3d2ddeac129d
Emmanuel Durin <emmanueldurin@gmail.com>
2021-04-29 17:50:31 +0200

    Add RequiredValidationRule

---
+++ HrManager/ValidationRules/RequiredValidationRule.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace HrManager.ValidationRules
+{
+  public class RequiredValidationRule : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+      if (value != null)
+      {
+        string s = value.ToString();
+        if (!string.IsNullOrEmpty(s))
+          return ValidationResult.ValidResult;
+      }
+      return new ValidationResult(false, "Saisie attendue");
+    }
+  }
+}
